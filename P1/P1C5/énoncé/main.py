27ea7a5526c89080289f60db6d9946dab4e31e6f
TiziaP <tizianopiserchia@gmail.com>
2024-07-13 15:32:20 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+    nom=tiziano
+age=16
+taille=1.85
+est_etudiant=true
+print(f"je m'appelle {nom},j'ai {age} ans et je suis etudiant {est_etudiant}")
+type(nom)
+type(age)
+type(est_etudiant)
